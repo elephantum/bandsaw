9875ef706bfe7fbd9cd8b79e5a6b506061d2d401
Andrey Tatarinov <elephantropy@gmail.com>
2013-09-14 13:56:06 +0400

    *

--- bandsaw.py
+++ bandsaw.py
@@ -1,3 +1,4 @@
+import sys
 import csv
 import datetime
 import re
@@ -6,30 +7,27 @@ import requests
 CSV_NAME = 'data.csv'
 
 
-def create_csv():
-    with open(CSV_NAME, 'wb') as csv_file:
-        writer = csv.writer(csv_file)
-        writer.writerow(['timestamp', 'skin_temp', 'air_temp', 'heartrate', 'steps', 'gsr', 'calories'])
+def create_csv(writer):
+    writer.writerow(['timestamp', 'skin_temp', 'air_temp', 'heartrate', 'steps', 'gsr', 'calories'])
 
 
-def append_to_csv(json):
+def append_to_csv(writer, json):
     """Takes JSON and flattens into CSVs."""
-    with open(CSV_NAME, 'ab') as csv_file:
-        writer = csv.writer(csv_file)
-        if 'endtime' not in json:
-            return
-        for i in range((json['endtime'] - json['starttime']) / json['interval']):
-            unix_time_utc = json['starttime'] + i*json['interval']
 
-            skin_temp = json['metrics']['skin_temp']['values'][i]
-            air_temp = json['metrics']['air_temp']['values'][i]
-            heartrate = json['metrics']['heartrate']['values'][i]
-            steps = json['metrics']['steps']['values'][i]
-            gsr = json['metrics']['gsr']['values'][i]
-            calories = json['metrics']['calories']['values'][i]
+    if 'endtime' not in json:
+        return
+    for i in range((json['endtime'] - json['starttime']) / json['interval']):
+        unix_time_utc = json['starttime'] + i*json['interval']
 
-            timestamp = datetime.datetime.fromtimestamp(unix_time_utc)
-            writer.writerow([timestamp, skin_temp, air_temp, heartrate, steps, gsr, calories])
+        skin_temp = json['metrics']['skin_temp']['values'][i]
+        air_temp = json['metrics']['air_temp']['values'][i]
+        heartrate = json['metrics']['heartrate']['values'][i]
+        steps = json['metrics']['steps']['values'][i]
+        gsr = json['metrics']['gsr']['values'][i]
+        calories = json['metrics']['calories']['values'][i]
+
+        timestamp = datetime.datetime.fromtimestamp(unix_time_utc)
+        writer.writerow([timestamp, skin_temp, air_temp, heartrate, steps, gsr, calories])
 
 
 def get_user_id(session):
@@ -46,11 +44,17 @@ def get_data(user_id, start_date, end_date):
     d = start_date
     while d <= end_date:
         url = 'https://app.mybasis.com/api/v1/chart/{0}.json?summary=true&interval=60&units=ms&start_date={1}&start_offset=-10800&end_offset=10800&heartrate=true&steps=true&calories=true&gsr=true&skin_temp=true&air_temp=true&bodystates=true'.format(user_id, d.strftime('%Y-%m-%d'))
+        print 'get url', url
         yield requests.get(url).json()
         d += delta
 
 
 if __name__ == '__main__':
-    create_csv()
-    for data in get_data('BASIS USER ID GOES HERE', datetime.datetime(2013, 8, 13), datetime.datetime(2013, 8, 28)):
-        append_to_csv(data)
+    with open(CSV_NAME, 'w+') as csv_file:
+        writer = csv.writer(csv_file)
+        create_csv(writer)
+
+        user_id = file('userid.txt').read()
+
+        for data in get_data(user_id, datetime.datetime(2013, 9, 9), datetime.datetime(2013, 9, 14)):
+            append_to_csv(writer, data)
